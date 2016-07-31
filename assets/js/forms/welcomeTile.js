b769c5f437babc7a8da96785de3294ce74bd8f93
Paul Lorsbach <paul@lorsbach.com>
2016-07-31 09:39:51 -0700

    add welcome tile form

---
+++ assets/js/forms/welcomeTile.js
@@ -0,0 +1,103 @@
+var welcomeTile = function () {
+
+    return {
+
+        //Comment Form
+        initWelcomeTileForm: function () {
+            // Validation
+            $("#welcome").validate({
+                // Rules for form validation
+                rules: {
+                    name: {
+                        required: false
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message: {
+                        required: false
+                    }
+                },
+
+                // Messages for form validation
+                messages: {
+                    email: {
+                        required: 'Enter your email address',
+                        email: 'Enter a VALID email'
+                    }
+                },
+
+                // Ajax form submition
+                submitHandler: function (form) {
+                    Stamplay.init("greathearts");
+
+                    var formData =
+                    {
+                        "recipient": $(form).find("#recipient").val(),
+                        "email": $(form).find("#email").val(),
+                        "name": $(form).find("#name").val(),
+                        "message": $(form).find("#message").val()
+                    };
+
+
+                    var userId;
+                    Stamplay.User.get({email: formData.email})
+                        .then(function (res) {
+                                if (res.data.length === 0) {//new user
+                                    Stamplay.Object("signups")
+                                        .save({"email": formData.email, "firstName": formData.name})
+                                        .then(function (res) {
+                                            Stamplay.User.get({email: formData.email})
+                                                .then(function (res) {
+                                                    sendTile(res.data[0].id);
+                                                }, function (err) {
+                                                    console.log(err);
+                                                });
+                                        }, function (err) {
+                                            console.log(err);
+                                        });
+                                }
+                                else {//user exists
+                                    sendTile(res.data[0].id);
+                                }
+                            },
+                            function (err) {
+                                console.log(err);
+                            });
+
+                    var sendTile = function (userId) {
+                        var data =
+                        {
+                            "content": {"message": formData.message},
+                            "giver_id": userId,
+                            "recipient_id": formData.recipient,
+                            "type": "5796692ce302c0b37c10bfd4"
+                        };
+
+                        Stamplay.Object("tiles")
+                            .save(data)
+                            .then(function (res) {
+                                $(form).addClass('submited');
+                                $(form).find('#submitButton').hide();
+                            }, function (err) {
+                                console.log(err);
+                            });
+                    };
+
+                    return false;//prevent default submit behavior
+
+                },
+
+                // Do not change code below
+                errorPlacement: function (error, element) {
+                    error.insertAfter(element.parent());
+                }
+            });
+
+
+        }
+
+    };
+
+}();
