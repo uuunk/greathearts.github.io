945db557ae94365ced9dbdd4ef8b8193e5e08d32
Gino Paul Capio <gcwokeup@users.noreply.github.com>
2017-01-03 14:23:20 -0600

    Remove api js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,1 @@
-// $(".testing").ready(function() {
-//   var request = $.ajax({
-//     url: 'http://localhost:3000/orgs_list',
-//     method: 'get',
-//     crossDomain: true
-//   });
 
-//   request.done(function(orgsJson){
-//   	var that = this
-//     var names = $.each(orgsJson, function( index, value){
-//       $(".testing).children().replaceWith("<li><figure><a href=" + value.link + "><img src="+ value.logo_link + " alt=>" + value.name + "<div class='img-hover'> <a href=" + value.link + "</div></a></figure></li>")
-//     });
-//     var childs =  $("#effect-2").children();
-//   });
-// })  
