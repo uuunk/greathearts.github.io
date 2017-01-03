96522d5d961013320b8a7b228bf9c341f27f51c9
Gino Capio <ginocapio@gmail.com>
2017-01-03 14:00:55 -0600

    Add API js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,15 @@
-/* Write here your custom javascript codes */
+$("#effect-2").ready(function() {
+  var request = $.ajax({
+    url: 'http://localhost:3000/orgs_list',
+    method: 'get',
+    crossDomain: true
+  });
+
+  request.done(function(orgsJson){
+  	var that = this
+    var names = $.each(orgsJson, function( index, value){
+      $("#effect-2").children().replaceWith("<li><figure><a href=" + value.link + "><img src="+ value.logo_link + " alt=>" + value.name + "<div class='img-hover'> <a href=" + value.link + "</div></a></figure></li>")
+    });
+    var childs =  $("#effect-2").children();
+  });
+})  
