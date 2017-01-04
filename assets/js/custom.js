02856fe62c1d06b34a12d2556549d21b0e878794
Gino Paul Capio <gcwokeup@users.noreply.github.com>
2017-01-03 18:40:00 -0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,17 @@
-$(".testing").ready(function() {
-  var request = $.ajax({
-    url: 'http://localhost:3000/orgs_list',
-    method: 'get',
-    crossDomain: true
-  });
+// $(".testing").ready(function() {
+//   var request = $.ajax({
+//     url: 'http://localhost:3000/orgs_list',
+//     method: 'get',
+//     crossDomain: true
+//   });
 
-  request.done(function(orgsJson){
-    $('.testing').children().remove();
-    $.each(orgsJson, function(index,value){
+//   request.done(function(orgsJson){
+//     $('.testing').children().remove();
+//     $.each(orgsJson, function(index,value){
 
-      $(".testing").append("<li><figure class='org-square flex-vertically-center-content'><img src=" + value.logo_link + " alt="+ value.name + "><div class='img-hover'><a href=" + value.link + " target='_blank'><h4>" + value.name + "</h4></a></div></figure></li>")
+//       $(".testing").append("<li><figure class='org-square flex-vertically-center-content'><img src=" + value.logo_link + " alt="+ value.name + "><div class='img-hover'><a href=" + value.link + " target='_blank'><h4>" + value.name + "</h4></a></div></figure></li>")
 
 
-    });
-  });
-})  
+//     });
+//   });
+// })  
