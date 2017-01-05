c12529b9a2de92d5b5fc67634e33f024f448213b
Gino Paul Capio <gcwokeup@users.noreply.github.com>
2017-01-05 16:22:00 -0600

    change class name and url

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
 $(".testing").ready(function() {
   var request = $.ajax({
-    url: 'https://great-hearts-app.herokuapp.com/api/v1/organizations',
+    url: 'https://app.greathearts.community/api/v1/organizations',
     method: 'get',
     crossDomain: true
   });
 
   request.done(function(orgsJson){
-    $('.testing').children().remove();
+    $('.orgs-squares').children().remove();
     $.each(orgsJson, function(index,value){
 
-      $(".testing").append("<li><figure class='org-square flex-vertically-center-content'><img src=" + value.logo_link + " alt="+ value.name + "><div class='img-hover'><a href=" + value.link + " target='_blank'><h4>" + value.name + "</h4></a></div></figure></li>")
+      $(".orgs-squares").append("<li><figure class='org-square flex-vertically-center-content'><img src=" + value.logo_link + " alt="+ value.name + "><div class='img-hover'><a href=" + value.link + " target='_blank'><h4>" + value.name + "</h4></a></div></figure></li>")
 
 
     });
