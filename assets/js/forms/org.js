d965362e4fa1e9c11c0682e3dea069ef2444fa61
Gino Capio <ginocapio@gmail.com>
2018-01-26 17:01:28 -0600

    add ein

--- assets/js/forms/org.js
+++ assets/js/forms/org.js
@@ -25,6 +25,10 @@ var OrgForm = function () {
                     {
                         required: true
                     },
+                    ein:
+                    {
+                        required: true
+                    },
                     email:
                     {
                         required: true,
